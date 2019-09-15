825ee1f57b33f70775c30153b5781be87ad2b004
prodes13 <ciobotaru.florin@gmail.com>
2019-09-15 14:15:49 +0300

    finished game of life

--- game-of-life/index.js
+++ game-of-life/index.js
@@ -14,30 +14,52 @@ function buildGrid() {
         .map(() => Math.floor(Math.random() * 2)));
 }
 
-const grid = buildGrid();
+let grid = buildGrid();
 
-render(grid);
+requestAnimationFrame(update);
+
+function update() {
+    grid = nextGen(grid);
+
+    render(grid);
+    
+    requestAnimationFrame(update);
+}
+
 
 // rendering next generation
 
 function nextGen(grid) {
     const nextGen = grid.map(arr => [...arr]);
-    
     for(let col = 0; col < grid.length; col++) {
         for(let row = 0; row < grid[col].length; row++) {
             const cell = grid[col][row];
-            const numNeighbours = 0;
+            let numNeighbours = 0;
             for(let i = -1; i < 2; i++) {
                 for(let j =-1; j < 2; j++) {
                     if( i === 0 && j === 0) {
                         continue;
                     }
-                    const currentNeighbour = grid[col+i][row+j];
-                    numNeighbours += currentNeighbour;
+                    const x_cell = col+i;
+                    const y_cell = row+j;
+
+                    if(x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
+                        const currentNeighbour = grid[col+i][row+j];
+                        numNeighbours += currentNeighbour;
+                    }
                 }
             }
+            if(cell === 1 && numNeighbours < 2) {
+                nextGen[col][row] = 0;
+            } else if(cell === 1 && numNeighbours > 3) {
+                nextGen[col][row] = 0;
+            } else if(cell === 0 && numNeighbours === 3) {
+                nextGen[col][row] = 1;
+            } 
         }
     }
+
+    return nextGen;
 }
 
 function render(grid) { 
