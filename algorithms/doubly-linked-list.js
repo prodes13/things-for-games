2cda450f595ff6467f0259a71b207f0dac9bd5e0
prodes13 <ciobotaru.florin@gmail.com>
2019-12-16 22:37:51 +0200

    almost done dll

--- algorithms/doubly-linked-list.js
+++ algorithms/doubly-linked-list.js
@@ -66,7 +66,60 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
-
+  // get(index) {
+  //     if(index < 0 || index > this.length) return null;
+  //     var count = 0;
+  //     var current = this.head;
+  //     while(count !== index) {
+  //       current = current.next;
+  //       count++;
+  //     }
+  //     return current;
+  // }
+  // optimized and unoptimized ^^^
+  get(index) {
+      if(index < 0 || index > this.length) return null;
+      var count, current;
+      if(index <= this.length/2) {
+        count = 0;
+        current = this.head;
+        while(count !== index) {
+          current = current.next;
+          count++;
+        }
+      } else {
+        count = this.length - 1;
+        current = this.tail;
+        while(count !== index) {
+          current = current.prev;
+          count--;
+        }
+      }
+      return current;
+  }
+  set(index, val) {
+    var foundNode = this.get(index);
+    if(foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if(index < 0 || index > this.length) return false;
+    if(index === 0) return this.unshift(val);
+    if(index === this.length) return this.push(val);
+    
+    var newNode = new Node(val);
+    var beforeNode = this.get(index-1);
+    var afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
 }
 
 var list = new DoublyLinkedList();
