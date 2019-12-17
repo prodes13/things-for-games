109cd518cfee3fb9dbf75ef25e77169615f7dc8b
prodes13 <ciobotaru.florin@gmail.com>
2019-12-17 20:41:11 +0200

    finished doubly linked list

--- algorithms/doubly-linked-list.js
+++ algorithms/doubly-linked-list.js
@@ -109,7 +109,7 @@ class DoublyLinkedList {
     if(index < 0 || index > this.length) return false;
     if(index === 0) return this.unshift(val);
     if(index === this.length) return this.push(val);
-    
+
     var newNode = new Node(val);
     var beforeNode = this.get(index-1);
     var afterNode = beforeNode.next;
@@ -120,8 +120,22 @@ class DoublyLinkedList {
     this.length++;
     return true;
   }
+  remove(index) {
+    if(index < 0 || index >= this.length) return false;
+    if(index === 0) return this.shift();
+    if(index === this.length - 1) return this.pop();
+    var removedNode = this.get(index);
+    removedNode.prev.next = removedNode.next;
+    removedNode.next.prev = removedNode.prev;
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return removedNode;
+  }
 }
 
 var list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
+list.push(3);
+list.push(4);
