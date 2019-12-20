d3ccb10a81531b47b8deeb4ee982f059177f9f06
prodes13 <ciobotaru.florin@gmail.com>
2019-12-20 23:58:52 +0200

    tree traversal finished

--- algorithms/tree-traversal.js
+++ algorithms/tree-traversal.js
@@ -85,6 +85,26 @@ class BinarySearchTree {
 
     return data;
   }
+  DFSPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder() {
+    var data = [];
+    function traverse(node) {
+      node.left && traverse(node.left);
+      data.push(node.value);
+      node.right && traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
