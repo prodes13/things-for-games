173f4d80c7f8af9289ec68431e952ededbb41a09
prodes13 <ciobotaru.florin@gmail.com>
2019-12-22 21:37:36 +0200

    longest common prefix

--- algorithms/longest-common-prefix.js
+++ algorithms/longest-common-prefix.js
@@ -18,4 +18,23 @@
 // All given inputs are in lowercase letters a-z.
 
 // HORIZONTAL METHOD
-// VERTICAL METHOD
+// VERTICAL METHOD 
+var longestCommonPrefix = function(strs) { 
+	// prefix =''
+	// Loop through characters(char, index)
+	//	Loop through strs (str)
+	// 		if str[index] != char
+	//			return prefix
+	//	prefix = prefix + char
+	// return prefix
+	let prefix = '';
+	if(strs.length === 0) return prefix;
+	for(let i = 0; i < strs[0].length; i++) {
+		const character = strs[0][i];
+		for(let j = 0; j < strs.length; j++) {
+			if(strs[j][i] != character) return prefix;
+		}
+		prefix = prefix + character;
+	}
+	return prefix;
+}
