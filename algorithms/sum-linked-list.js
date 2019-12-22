8d98d0177ba7d8dc25960ccec0b66ede079fc5a9
prodes13 <ciobotaru.florin@gmail.com>
2019-12-22 11:37:46 +0200

    sum a linked list

--- algorithms/sum-linked-list.js
+++ algorithms/sum-linked-list.js
@@ -8,3 +8,21 @@
 // Output: 7 -> 0 -> 8
 // Explanation: 342 + 465 = 807.
 
+var addTwoNumbers = function(l1, l2) {
+	const a = [];
+	let carryOver = 0;
+	while(l1 != null || l2 != null) {
+		let v1 = 0;
+		let v2 = 0;
+		l1 && v1 = l1.val;
+		l2 && v2 = l2.val;
+
+		let sum = v1 + v2 + carryOver;
+		carryOver = Math.floor(sum / 10);
+		sum = sum % 10;
+		a.push(sum);
+
+		l1 != null && l1 = l1.next;
+		l2 != null && l2 = l2.next;
+	}
+}
