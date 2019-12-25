3d4f3c55b06ad9ef905b54a43dbb15744fc12757
prodes13 <ciobotaru.florin@gmail.com>
2019-12-25 23:09:34 +0200

    Dijkstra finished

--- algorithms/weighted-graph.js
+++ algorithms/weighted-graph.js
@@ -1,16 +1,3 @@
-class WeightedGraph {
-	constructor() {
-		this.adjacencyList = {};
-	}
-	addVertex(vertex) {
-		if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-	}
-	addEdge(vertex1, vertex2, weight) {
-		this.adjacencyList[vertex1].push({ node: vertex2, weight });
-		this.adjacencyList[vertex2].push({ node: vertex1, weight });
-	}
-}
-
 class PriorityQueue {
 	constructor() {
 		this.values = [];
@@ -26,3 +13,85 @@ class PriorityQueue {
 		this.values.sort((a, b) => a.priority - b.priority);
 	}
 }
+
+class WeightedGraph {
+	constructor() {
+		this.adjacencyList = {};
+	}
+	addVertex(vertex) {
+		if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+	}
+	addEdge(vertex1, vertex2, weight) {
+		this.adjacencyList[vertex1].push({ node: vertex2, weight });
+		this.adjacencyList[vertex2].push({ node: vertex1, weight });
+	}
+	Dijkstra(start, finish) {
+		const nodes = new PriorityQueue();
+		const distances = {};
+		const previous = {};
+		let path = [];
+		let smallest;
+		// build up initial state
+		for(let vertex in this.adjacencyList) {
+			if(vertex === start) {
+				distances[vertex] = 0;
+				nodes.enqueue(vertex, 0);
+			} else {
+				distances[vertex] = Infinity;
+				nodes.enqueue(vertex, Infinity);
+			}
+			previous[vertex] = null;
+		}
+		// as long as there is something to visit
+		while(nodes.values.length) {
+			smallest = nodes.dequeue().val;
+			if(smallest === finish) {
+				// WE ARE DONE
+				// BUILD PATH TO RETURN AT END
+				while(previous[smallest]) {
+					path.push(smallest);
+					smallest = previous[smallest];
+				}
+				break;
+			}
+			if(smallest || distances[smallest] !== Infinity) {
+				for(let neighbor in this.adjacencyList[smallest]) {
+					// find neighboring node
+					let nextNode = this.adjacencyList[smallest][neighbor];
+					// console.log(nextNode);
+					// calculate new distance to neighboring node
+					let candidate = distances[smallest] + nextNode.weight;
+					if(candidate < distances[nextNode.node]) {
+						// updating new smallest dist to neighbor
+						distances[nextNode.node] = candidate;
+						// updating previous - how we got to neighbor
+						previous[nextNode.node] = smallest;
+						// enqueue in priority queue with new priority
+						nodes.enqueue(nextNode.node, candidate);
+					}
+				}
+			}
+		}
+		return path.concat(smallest).reverse();
+	}
+	
+}
+
+var graph = new WeightedGraph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A","B", 4);
+graph.addEdge("A","C", 2);
+graph.addEdge("B","E", 3);
+graph.addEdge("C","D", 2);
+graph.addEdge("C","F", 4);
+graph.addEdge("D","E", 3);
+graph.addEdge("D","F", 1);
+graph.addEdge("E","F", 1);
+
+graph.Dijkstra("A", "E");
